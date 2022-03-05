44b1da147faf9337fc2eb7d0fb42e8d2c56752ff
Sergey <Duxxless53@ya.ru>
2022-03-05 22:31:25 +0300

    Initial Commit

---
+++ AlamofireRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireRequest
+//
+//  Created by Duxxless on 05.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
